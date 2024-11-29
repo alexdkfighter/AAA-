50f4915052b5f912578f925ffb4a42f458f4dfc1
alexdkfighter <13695242581@163.com>
2024-11-29 20:57:09 +0800

    添加了测试项目并且改变了项目的位置，好看一点

---
+++ AAA-/test_tools/test_tools.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testtools
+{
+	TEST_CLASS(testtools)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
